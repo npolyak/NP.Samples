f066132ca008cdf6e2e08d46093c8a77d35c0be0
npolyak <nick.polyak@verizon.net>
2022-12-15 21:58:54 -0500

    updated the cell

--- DependencyInjectionSamples/Implementations/FactoryMethods.cs
+++ DependencyInjectionSamples/Implementations/FactoryMethods.cs
@@ -6,7 +6,6 @@ namespace NP.Samples.Implementations
     [HasRegisterMethods]
     public static class FactoryMethods
     {
-        [RegisterMethod(resolutionKey:"TheAddress", ResolvingType = typeof(IAddress))]
         public static IAddress CreateAddress()
         {
             return new Address();
