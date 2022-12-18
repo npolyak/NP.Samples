87268758adb61e70877a52eacd98dc7184a9b3e1
npolyak <nick.polyak@verizon.net>
2022-12-17 23:45:59 -0500

    fixed the tests

--- DependencyInjectionSamples/Implementations/FactoryMethods.cs
+++ DependencyInjectionSamples/Implementations/FactoryMethods.cs
@@ -7,20 +7,20 @@ namespace NP.Samples.Implementations
     [HasRegisterMethods]
     public static class FactoryMethods
     {
-        [RegisterMethod(resolutionKey:"TheAddress")]
+        [RegisterMethod(resolutionKey: "TheAddress")]
         public static IAddress CreateAddress()
         {
-            return new Address();
+            return new Address { City = "Providence" };
         }
 
-        [RegisterMethod(isSingleton:true, resolutionKey:"TheManager")]  
+        [RegisterMethod(isSingleton: true, resolutionKey: "TheManager")]
         public static IPerson CreateManager([Inject(resolutionKey: "TheAddress")] IAddress address)
         {
             return new Person { PersonName = "Joe Doe", Address = address };
         }
 
-        [RegisterMethod(resolutionKey:"TheOrg")]
-        public static IOrg CreateOrg([Inject(resolutionKey:"TheManager")] IPerson person)
+        [RegisterMethod(resolutionKey: "TheOrg")]
+        public static IOrg CreateOrg([Inject(resolutionKey: "TheManager")] IPerson person)
         {
             return new Org { OrgName = "Other Department Store", Manager = person };
         }
