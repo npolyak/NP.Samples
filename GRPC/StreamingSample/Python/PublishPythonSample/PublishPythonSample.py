cef3ed1c3d8cf6bee160a8b72566c2ec96cf320e
npolyak <nick.polyak@verizon.net>
2023-01-22 20:48:45 -0500

    created publishing python test.

--- GRPC/StreamingSample/Python/PublishPythonSample/PublishPythonSample.py
+++ GRPC/StreamingSample/Python/PublishPythonSample/PublishPythonSample.py
@@ -1,1 +1,26 @@
+import logging;
 
+import grpc
+
+import grpc_tools.protoc
+
+
+logging.basicConfig()
+
+grpc_tools.protoc.main([
+    'grpc_tools.protoc',
+    '-I{}'.format("../../Protos/."),
+    '--python_out=.',
+    '--grpc_python_out=.',
+    '../../Protos/service.proto'
+])
+
+import service_pb2;
+import service_pb2_grpc;
+
+
+channel = grpc.insecure_channel('localhost:5555')
+
+stub = service_pb2_grpc.RelayServiceStub(channel);
+
+response = stub.Publish(service_pb2.Message(msg='Publish from Python Client'));
