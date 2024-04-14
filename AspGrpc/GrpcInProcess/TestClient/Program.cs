c1aa8c03fa0752d5f84f8c41c291aaae4e5c6683
npolyak <nick.polyak@verizon.net>
2024-04-14 12:04:02 -0400

    created more client samples

--- AspGrpc/GrpcInProcess/TestClient/Program.cs
+++ AspGrpc/GrpcInProcess/TestClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using simple;
 
 // get the channel connecting the client to the server
@@ -8,8 +9,42 @@ var channel =
 // create the GreeterClient service
 var client = new Greeter.GreeterClient(channel);
 
+
+
+Console.WriteLine($"Unary server call sample:");
 // call SetHello RPC on the server asynchronously and wait for the reply.
 var reply =
     await client.SayHelloAsync(new HelloRequest { Name = "C# Client" });
 
 Console.WriteLine("Greeting: " + reply.Msg);
+
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine($"Streaming Server Sample:");
+
+var serverStreamingCall = client.SayManyHellos(new HelloRequest { Name = "C# Client" });
+
+await foreach(var response in serverStreamingCall.ResponseStream.ReadAllAsync())
+{
+    Console.WriteLine("Greeting: " + response.Msg);
+}
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine($"Streaming Client Sample:");
+
+var clientSreamingCall = client.SayHelloToMany();
+
+for(int i = 0; i < 3;  i++)
+{
+    await clientSreamingCall.RequestStream.WriteAsync(new HelloRequest { Name = $"Client_{i + 1}" });
+}
+
+await clientSreamingCall.RequestStream.CompleteAsync();
+
+
+var clientStreamingResponse = await clientSreamingCall;
+Console.WriteLine(clientStreamingResponse.Msg);
