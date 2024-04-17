e65d95af8a473a67748d479149af584a21553d3b
npolyak <nick.polyak@verizon.net>
2024-04-17 15:51:45 -0400

    added Razor/JS/HTML client

--- AspGrpc/GreeterImpl/GreeterImplementation.cs
+++ AspGrpc/GreeterImpl/GreeterImplementation.cs
@@ -12,21 +12,49 @@ namespace GrpcServerProcess
             return new HelloReply { Msg = $"Hello {name}!!!" };
         }
 
+
+        private async Task ServerStreamHelloRepliesImpl
+        (
+            HelloRequest request, 
+            bool throwException, 
+            IServerStreamWriter<HelloReply> responseStream, 
+            ServerCallContext context)
+        {
+            string name = request.Name;
+
+            for (int i = 0; i < 30; i++)
+            {
+                await Task.Delay(200);
+                await responseStream.WriteAsync(new HelloReply { Msg = $"Hello {name} {i + 1}" });
+                context.CancellationToken.ThrowIfCancellationRequested();
+
+                if (i == 10 && throwException)
+                {
+                    throw new RpcException(new Status(StatusCode.Internal, "Error Status Detail (for the client)"), "ERROR: Cannot Continue with streaming (server error)!");
+                }
+            }
+        }
+
+
         public override async Task ServerStreamHelloReplies
         (
             HelloRequest request, 
             IServerStreamWriter<HelloReply> responseStream, 
             ServerCallContext context)
         {
-            string name = request.Name;
-
-            for(int i = 0; i < 10; i++)
-            {
-                await responseStream.WriteAsync(new HelloReply { Msg = $"Hello {name} {i + 1}" });
-                await Task.Delay(500);
-                context.CancellationToken.ThrowIfCancellationRequested();
-            }
+            await ServerStreamHelloRepliesImpl(request, false, responseStream, context);
         }
+
+
+        public override async Task ServerStreamHelloRepliesWithError
+        (
+            HelloRequest request,
+            IServerStreamWriter<HelloReply> responseStream,
+            ServerCallContext context)
+        {
+            await ServerStreamHelloRepliesImpl(request, true, responseStream, context);
+        }
+
 
         public override async Task<HelloReply> ClientStreamHelloRequests(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
         {
---
+++ AspGrpc/Protos/README.txt
@@ -0,0 +1,3 @@
+﻿Command to generate grpc javascript client proxy code:
+
+protoc -I=. Greeter.proto --js_out=import_style=commonjs:..\AspGrpcServerWithRazorClient\wwwroot\dist --grpc-web_out=import_style=commonjs,mode=grpcwebtext:..\AspGrpcServerWithRazorClient\wwwroot\dist
