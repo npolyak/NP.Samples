9f8661f64eba1927fcf8c6103c74325de2567204
npolyak <nick.polyak@verizon.net>
2024-04-18 15:11:20 -0400

    CheckPoint

--- AspGrpc/MyTestsWebSite/Pages/AspGrpcTests/HtmlGrpcTestPage.cshtml.cs
+++ AspGrpc/MyTestsWebSite/Pages/AspGrpcTests/HtmlGrpcTestPage.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AspGrpcServerWithRazorClient.AspGrpcTests.Pages
+namespace MyTestsWebSite.AspGrpcTests.Pages
 {
     public class HtmlGrpcTestPageModel : PageModel
     {
