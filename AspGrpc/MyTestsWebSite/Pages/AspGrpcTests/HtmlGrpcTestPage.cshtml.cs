981c77cb6f3e4087c61ee1ea40f41675ae21e69d
npolyak <nick.polyak@verizon.net>
2024-04-18 14:21:19 -0400

    added testing website

---
+++ AspGrpc/MyTestsWebSite/Pages/AspGrpcTests/HtmlGrpcTestPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspGrpcServerWithRazorClient.AspGrpcTests.Pages
+{
+    public class HtmlGrpcTestPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AspGrpc/MyTestsWebSite/webpack.config.js
@@ -0,0 +1,10 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './wwwroot/dist/client.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'main.js',
+    },
+};
