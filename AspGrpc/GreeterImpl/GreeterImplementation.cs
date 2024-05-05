f7a2dc0d4fdef00999a0bfc77749154ffb2db49e
npolyak <nick.polyak@verizon.net>
2024-05-05 19:02:59 -0400

    added comments

--- AspGrpc/GreeterImpl/GreeterImplementation.cs
+++ AspGrpc/GreeterImpl/GreeterImplementation.cs
@@ -1,111 +1,110 @@
 ﻿using Grpc.Core;
 using simple;
 
-namespace GrpcServerProcess
+namespace GrpcServerProcess;
+
+public class GreeterImplementation : simple.Greeter.GreeterBase
 {
-    public class GreeterImplementation : simple.Greeter.GreeterBase
+    public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
-        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            string name = request.Name;
+        string name = request.Name;
 
-            return new HelloReply { Msg = $"Hello {name}!!!" };
+        return new HelloReply { Msg = $"Hello {name}!!!" };
+    }
+
+
+    private async Task ServerStreamHelloRepliesImpl
+    (
+        HelloRequest request,
+        bool throwException,
+        IServerStreamWriter<HelloReply> responseStream,
+        ServerCallContext context)
+    {
+        string name = request.Name;
+
+        for (int i = 0; i < 20; i++)
+        {
+            await Task.Delay(200);
+            await responseStream.WriteAsync(new HelloReply { Msg = $"Hello {name} {i + 1}" });
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            if (i == 10 && throwException)
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "Error Status Detail (for the client)"), "ERROR: Cannot Continue with streaming (server error)!");
+            }
+        }
+    }
+
+
+    public override async Task ServerStreamHelloReplies
+    (
+        HelloRequest request,
+        IServerStreamWriter<HelloReply> responseStream,
+        ServerCallContext context)
+    {
+        await ServerStreamHelloRepliesImpl(request, false, responseStream, context);
+    }
+
+
+    public override async Task ServerStreamHelloRepliesWithError
+    (
+        HelloRequest request,
+        IServerStreamWriter<HelloReply> responseStream,
+        ServerCallContext context)
+    {
+        await ServerStreamHelloRepliesImpl(request, true, responseStream, context);
+    }
+
+
+    public override async Task<HelloReply> ClientStreamHelloRequests(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
+    {
+        string message = "Hello ";
+
+        bool first = true;
+        await foreach (var inputMessage in requestStream.ReadAllAsync())
+        {
+            if (!first)
+            {
+                message += ", ";
+            }
+
+            message += inputMessage.Name;
+            first = false;
         }
 
+        return new HelloReply { Msg = message };
+    }
 
-        private async Task ServerStreamHelloRepliesImpl
-        (
-            HelloRequest request, 
-            bool throwException, 
-            IServerStreamWriter<HelloReply> responseStream, 
-            ServerCallContext context)
+    public override async Task ClientAndServerStreamingTest(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+    {
+        //await foreach (var inputMessage in requestStream.ReadAllAsync())
+        //{
+        //    string name = inputMessage.Name;
+
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        string msg = $"Hello {name}_{i + 1}";
+        //        Console.WriteLine(msg);
+        //        await responseStream.WriteAsync(new HelloReply { Msg = msg });
+        //        await Task.Delay(200);
+
+        //        context.CancellationToken.ThrowIfCancellationRequested();
+        //    }
+        //}
+
+        await Parallel.ForEachAsync(requestStream.ReadAllAsync(), async (inputMessage, ct) =>
         {
-            string name = request.Name;
+            string name = inputMessage.Name;
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 4; i++)
             {
+                string msg = $"Hello {name}_{i + 1}";
+                Console.WriteLine(msg);
+                await responseStream.WriteAsync(new HelloReply { Msg = msg });
                 await Task.Delay(200);
-                await responseStream.WriteAsync(new HelloReply { Msg = $"Hello {name} {i + 1}" });
+
                 context.CancellationToken.ThrowIfCancellationRequested();
-
-                if (i == 10 && throwException)
-                {
-                    throw new RpcException(new Status(StatusCode.Internal, "Error Status Detail (for the client)"), "ERROR: Cannot Continue with streaming (server error)!");
-                }
             }
-        }
-
-
-        public override async Task ServerStreamHelloReplies
-        (
-            HelloRequest request, 
-            IServerStreamWriter<HelloReply> responseStream, 
-            ServerCallContext context)
-        {
-            await ServerStreamHelloRepliesImpl(request, false, responseStream, context);
-        }
-
-
-        public override async Task ServerStreamHelloRepliesWithError
-        (
-            HelloRequest request,
-            IServerStreamWriter<HelloReply> responseStream,
-            ServerCallContext context)
-        {
-            await ServerStreamHelloRepliesImpl(request, true, responseStream, context);
-        }
-
-
-        public override async Task<HelloReply> ClientStreamHelloRequests(IAsyncStreamReader<HelloRequest> requestStream, ServerCallContext context)
-        {
-            string message = "Hello ";
-
-            bool first = true;
-            await foreach (var inputMessage in requestStream.ReadAllAsync())
-            {
-                if (!first)
-                {
-                    message += ", ";
-                }
-
-                message += inputMessage.Name;
-                first = false;
-            }
-
-            return new HelloReply { Msg = message };
-        }
-
-        public override async Task ClientAndServerStreamingTest(IAsyncStreamReader<HelloRequest> requestStream, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
-        {
-            //await foreach (var inputMessage in requestStream.ReadAllAsync())
-            //{
-            //    string name = inputMessage.Name;
-
-            //    for (int i = 0; i < 4; i++)
-            //    {
-            //        string msg = $"Hello {name}_{i + 1}";
-            //        Console.WriteLine(msg);
-            //        await responseStream.WriteAsync(new HelloReply { Msg = msg });
-            //        await Task.Delay(200);
-
-            //        context.CancellationToken.ThrowIfCancellationRequested();
-            //    }
-            //}
-
-            await Parallel.ForEachAsync(requestStream.ReadAllAsync(), async (inputMessage, ct) =>
-            {
-                string name = inputMessage.Name;
-
-                for (int i = 0; i < 4; i++)
-                {
-                    string msg = $"Hello {name}_{i + 1}";
-                    Console.WriteLine(msg);
-                    await responseStream.WriteAsync(new HelloReply { Msg = msg });
-                    await Task.Delay(200);
-
-                    context.CancellationToken.ThrowIfCancellationRequested();
-                }
-            });
-        }
+        });
     }
 }
--- AspGrpc/GrpcServerProcess/Program.cs
+++ AspGrpc/GrpcServerProcess/Program.cs
@@ -1,11 +1,18 @@
 using GrpcServerProcess;
 
+// create builder
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
+
+// add grpc service
 builder.Services.AddGrpc();
 
+// create the Kestrel application
 var app = builder.Build();
 
+// specifies GreeterImplementation as the gRPC service to run and 
+// channel to it all the requests that specify 55003 port
 app.MapGrpcService<GreeterImplementation>().RequireHost("*:55003");
 
+// runs Kestrel server. 
 app.Run();
