362040d8acf2b8aef55f7e3d4e278c99284c6fa4
npolyak <nick.polyak@verizon.net>
2024-05-07 17:20:42 -0400

    removed unneeded stuff. fixed Index.cshtml and _Layout.cshtml

---
+++ AspGrpc/AspGrpcServer/wwwroot/mainForAvalonia.js
@@ -0,0 +1,13 @@
+﻿import { dotnet } from './_framework/dotnet.js'
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+const { setModuleImports, getAssemblyExports, getConfig, runMain } = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+const config = getConfig();
+
+await runMain(config.mainAssemblyName, [window.location.origin]);
