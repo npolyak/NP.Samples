8dce95682184526f504079f58142074672d69bef
npolyak <nick.polyak@verizon.net>
2024-05-08 14:14:22 -0400

    added comments

--- AspGrpc/AvaGrpcClient/AvaGrpcClient/Views/MainView.axaml.cs
+++ AspGrpc/AvaGrpcClient/AvaGrpcClient/Views/MainView.axaml.cs
@@ -24,10 +24,11 @@ public partial class MainView : UserControl
         var channel =
             GrpcChannel.ForAddress
             (
-                CommonData.Url!,
+                CommonData.Url!, // server address
                 new GrpcChannelOptions
                 {
-                    HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+                    // indicates the browser grpc connection
+                    HttpHandler = new GrpcWebHandler(new HttpClientHandler()) 
                 });
 
         // create the GreeterClient service
@@ -48,45 +49,60 @@ public partial class MainView : UserControl
 
     private async void TestUnaryHelloButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+        // call simple single request/single response SayHello service
         var reply =
             await _greeterGrpcClient.SayHelloAsync(new HelloRequest { Name = GreetingName });
+
+        // display the result
         HelloResultText.Text = reply.Msg;
     }
 
     private async void TestStreamingServerButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+        // set initial values to empty strings
         StreamingServerResultsText.Text = string.Empty;
         StreamingErrorText.Text = string.Empty;
         try
         {
-            var serverStreamingCall = _greeterGrpcClient.ServerStreamHelloReplies(new HelloRequest { Name = GreetingName });
-            await foreach (var response in serverStreamingCall.ResponseStream.ReadAllAsync(_serverStreamCancellationTokenSource.Token))
+            // get the server stream container
+            var serverStreamingResponsesContainer = 
+                _greeterGrpcClient.ServerStreamHelloReplies(new HelloRequest { Name = GreetingName });
+            
+            // foreach of the async responses from the server
+            await foreach (var response in serverStreamingResponsesContainer.ResponseStream.ReadAllAsync(_serverStreamCancellationTokenSource.Token))
             {
+                // change the text of the TextBox
                 StreamingServerResultsText.Text = response.Msg;
             }
         }
         catch(RpcException exception)
         {
+            // if an exception is throws, show the exception message
             StreamingErrorText.Text = $"ERROR: {exception.Message}";
         }
     }
 
     private async void TestStreamingServerWithErrorButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+        // set initial values to empty strings
         StreamingServerResultsText.Text = string.Empty;
         StreamingErrorText.Text = string.Empty;
 
         try
         {
+            // foreach of the async responses from the server
             var serverStreamingCall = _greeterGrpcClient.ServerStreamHelloRepliesWithError(new HelloRequest { Name = GreetingName });
 
+            // foreach of the async responses from the server
             await foreach (var response in serverStreamingCall.ResponseStream.ReadAllAsync(_serverStreamCancellationTokenSource.Token))
             {
+                // change the text of the TextBox
                 StreamingServerResultsText.Text = response.Msg;
             }
         }
         catch (RpcException exception)
         {
+            // if an exception is throws, show the exception message
             StreamingErrorText.Text = $"ERROR: {exception.Message}";
         }
     }
@@ -94,53 +110,82 @@ public partial class MainView : UserControl
 
     private void TestStreamingServerCancelButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+        // send signal to the server to cancel streaming 
         _serverStreamCancellationTokenSource?.Cancel();
+
+        // change the streaming token
         _serverStreamCancellationTokenSource = new CancellationTokenSource();
     }
 
+    // test streaming client to the server
+    // unfortunately the client in a browser does not stream,
+    // it accumulates all the messages on the browser, and
+    // sends them together after the client indicates the end of streaming
     private async void TestStreamingClientButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
+        // reset the UI text to emtpy
         StreamingClientResultsText.Text = string.Empty;
 
-        var clientSreamingCall = _greeterGrpcClient.ClientStreamHelloRequests();
+        // create stream container
+        var clientStreamContainer = _greeterGrpcClient.ClientStreamHelloRequests();
 
         for (int i = 0; i < 5; i++)
         {
-            await clientSreamingCall.RequestStream.WriteAsync(new HelloRequest { Name = $"Client_{i + 1}" });
+            // push the messages into the request streams
+            await clientStreamContainer.RequestStream.WriteAsync(new HelloRequest { Name = $"Client_{i + 1}" });
 
             await Task.Delay(1000);
         }
 
-        await clientSreamingCall.RequestStream.CompleteAsync();
-        var clientStreamingResponse = await clientSreamingCall;
+        // indicate the completion of the client streaming
+        // Unfortunately it is only at this point that all the client messages
+        // will be sent to the server. Essentially that means that there is 
+        // no client streaming
+        await clientStreamContainer.RequestStream.CompleteAsync();
 
+        // get the server response
+        var clientStreamingResponse = await clientStreamContainer;
+
+        // set the visual text of the server response
         StreamingClientResultsText.Text = clientStreamingResponse.Msg;
     }
 
-
+    // Bi-Directional (Client and Server) streaming test. 
+    // Unfortunately the client messages are accumulated on the client side
+    // and sent to the server together only after
+    // the client indicates the end of streaming
+    // Which essentially means that there is no client streaming
     private async void TestStreamingClientServerButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        var clientServerStreamingCall = _greeterGrpcClient.ClientAndServerStreamingTest();
+        // create server and client streamd container
+        var clientServerStreamContainer = _greeterGrpcClient.ClientAndServerStreamingTest();
 
+        // reset the server reply text to empty
         StreamingClientServerResultsText.Text = string.Empty;
 
+        // create an async task to asynchronously process the server responses 
+        // as they come
         var readTask = Task.Run(async () =>
         {
-            await foreach (var reply in clientServerStreamingCall.ResponseStream.ReadAllAsync())
+            await foreach (var reply in clientServerStreamContainer.ResponseStream.ReadAllAsync())
             {
+                // for each server response we assing it to show in the client browser
                 await Dispatcher.UIThread.InvokeAsync(() => { StreamingClientServerResultsText.Text += reply.Msg + "\n"; });
             }
         });
 
+        // push 3 client requests into the stream
+        // unfortunately they'll accumulated on the client and sent together 
+        // only after the client call RequestStream.CompleteAsync() method
         for (int i = 0; i < 3; i++)
         {
-            await clientServerStreamingCall.RequestStream.WriteAsync(new HelloRequest { Name = $"Client_{i + 1}" });
+            await clientServerStreamContainer.RequestStream.WriteAsync(new HelloRequest { Name = $"Client_{i + 1}" });
 
             await Task.Delay(1000);
         }
 
-
-        await Task.WhenAll(readTask, clientServerStreamingCall.RequestStream.CompleteAsync());
+        // wait for both the client and the server processing to finish.
+        await Task.WhenAll(readTask, clientServerStreamContainer.RequestStream.CompleteAsync());
     }
 
 }
