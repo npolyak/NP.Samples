915e55b0d6f359b5f7e01d6bf190206849b30eac
npolyak <nick.polyak@verizon.net>
2024-05-10 14:32:45 -0400

    created a well documented WASM sample

---
+++ Wasm/JSCallingDotNetSample/JSCallingDotNetSample/wwwroot/wasmLoader.js
@@ -0,0 +1,25 @@
+﻿import { dotnet } from './_framework/dotnet.js'
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+// get the objects needed to run exported C# code
+const { getAssemblyExports, getConfig } =
+    await dotnet.create();
+
+// config contains the web-site configurations
+const config = getConfig();
+
+// exports contain the methods exported by C#
+const exports = await getAssemblyExports(config.mainAssemblyName);
+
+// we call the exported C# method Greeter.JSInteropCallsContainer.Greet
+// passing to it an array of names
+const text = exports.Greeter.JSInteropCallsContainer.Greet(['Nick', 'Joe', 'Bob']);
+
+// logging the result of Greet method call
+console.log(text);
+
+// adding the result of Greet method call to the inner text of 
+// an element out id "out"
+document.getElementById("out").innerText = text;
