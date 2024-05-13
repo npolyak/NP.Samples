ce5b0a9b09b3f51f618fd4942b64ac252d44e171
npolyak <nick.polyak@verizon.net>
2024-05-13 19:28:28 -0400

    some minor changes.

--- Wasm/AvaInBrowserSample/AvaInBrowserSample/wwwroot/wasmRunner.js
+++ Wasm/AvaInBrowserSample/AvaInBrowserSample/wwwroot/wasmRunner.js
@@ -4,8 +4,8 @@ const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
 
 const dotnetRuntime = await dotnet
-    .withDiagnosticTracing(false)
-    .withApplicationArgumentsFromQuery()
+    .withDiagnosticTracing(false) // some extra methods
+    .withApplicationArgumentsFromQuery() // some extra methods
     .create();
 
 const config = dotnetRuntime.getConfig();
