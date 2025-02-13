f110fdd4cf2a6feefa49f88e358c8bd0cbf2974c
npolyak <nick.polyak@verizon.net>
2025-02-12 23:05:32 -0500

    added TestTimeSpannedStreamFromCollection

--- RxStreams/CreatingAndManipulatingRxStreams/StreamFromCollection.cs
+++ RxStreams/CreatingAndManipulatingRxStreams/StreamFromCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Reactive.Linq;
+﻿using System.Reactive;
+using System.Reactive.Linq;
 using Xunit;
 
 namespace CreatingAndManipulatingRxStreams;
@@ -9,9 +10,11 @@ public class StreamFromCollection
     public static void TestStreamFromCollection()
     {
         // source collection consists of int numbers 1 to 10
-        int[] sourceCollection1_10 = Enumerable.Range(1, 10).ToArray();
+        int[] sourceCollection1_10 = 
+                Enumerable.Range(1, 10).ToArray();
 
-        IObservable<int> sourceObservable = sourceCollection1_10.ToObservable();
+        IObservable<int> sourceObservable = 
+                            sourceCollection1_10.ToObservable();
 
         // observable emitting squares for only even numbers
         // out of the source
@@ -30,7 +33,8 @@ public class StreamFromCollection
         // however complex they are do not consume any
         // heap resources and cannot result in a memory leak.
         // Only subscription needs to be dispose of 
-        // (which we do automatically by providing the 'using' clause).
+        // (which we do automatically by providing
+        //  the 'using' clause).
         using var subscriptionDisposable =
             evenSquaresObservable
                 .Subscribe(i => resultEvenSquaresCollection.Add(i));
@@ -43,6 +47,77 @@ public class StreamFromCollection
 
         // assert that the result and expectedResults collections
         // are the same.
+        Assert.True
+        (
+            resultEvenSquaresCollection
+                .SequenceEqual(expectedResultsCollection)
+        );
+    }
+
+    [Fact]
+    public static async Task TestTimeSpannedStreamFromCollection()
+    {
+        // source collection consists of int numbers 1 to 10
+        int[] sourceCollection1_10 = 
+                Enumerable.Range(1, 10).ToArray();
+
+
+        bool completed = false;
+        IObservable<int> evenSquaresObservable =
+                            sourceCollection1_10
+                                .ToObservable()
+                                .Where(i => i % 2 == 0)
+                                .Select(i => i * i);
+
+        // create an infinite stream emitting 
+        // empty data (Unit.Default stands for empty data)
+        // every second
+        IObservable<Unit> eachSecondObservable =
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                      .Select(_ => Unit.Default);
+
+        // Zip operator matches the data emitted from
+        // two or more observable streams - 1st to 1st, 2nd to 2nd
+        // etc.
+        // Emission number N happens after EACH one of its
+        // observable streams produces N-th emission. 
+        //
+        // The Zipped stream completes when ANY of its 
+        // observable streams complete.
+        IObservable<int> timeSpannedEvenSquaresObservable =
+            evenSquaresObservable
+                .Zip
+                (
+                    eachSecondObservable,
+
+                    // we only need values from 
+                    // the first sequence - evenSquaresObservable
+                    // From the second sequence 
+                    // (eachSecondObservable) we only 
+                    // need time of emission
+                    (i, unit) => i
+                );
+
+        var resultEvenSquaresCollection = new List<int>();
+
+        using var subscriptionDisposable =
+                        timeSpannedEvenSquaresObservable
+                        .Subscribe
+                        (
+                            i => resultEvenSquaresCollection.Add(i),
+                            () => completed = true
+                        );
+
+        // delay the testing until completed flag is 
+        // switched to true
+        while (! completed)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+
+        int[] expectedResultsCollection =
+            [2 * 2, 4 * 4, 6 * 6, 8 * 8, 10 * 10];
+
         Assert.True(resultEvenSquaresCollection.SequenceEqual(expectedResultsCollection));
     }
 }
