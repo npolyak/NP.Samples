921d8640ea1460178f12f7f47ebbf1d2e6250164
npolyak <nick.polyak@verizon.net>
2025-02-13 20:35:50 -0500

    added StreamFromAsyncEnumerable.cs

---
+++ RxStreams/CreatingAndManipulatingRxStreams/StreamFromAsyncEnumerable.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace CreatingAndManipulatingRxStreams;
+
+public static class StreamFromAsyncEnumerable
+{
+    public static async IAsyncEnumerable<int> 
+        GenerateAsyncEnumerable()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            // one second delay
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            yield return i;
+        }
+    }
+
+
+    [Fact]
+    public static async Task TestStreamFromAsyncEnumerable()
+    {
+        IAsyncEnumerable<int> asyncEnumerable = 
+            GenerateAsyncEnumerable();
+
+        IObservable<int> observable = 
+            asyncEnumerable.ToObservable();
+
+        var resultEvenSquaresCollection = new List<int>();
+        bool completed = false;
+
+        using var subscribeDisposable =
+            observable
+                .Where(i => i % 2 == 0)
+                .Select(i => i * i)
+                .Subscribe
+                (
+                    onNext:
+                        i => resultEvenSquaresCollection.Add(i),
+
+                    onCompleted:
+                        () => completed = true
+                );
+
+        // delay the testing until completed flag is 
+        // switched to true
+        while (!completed)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
