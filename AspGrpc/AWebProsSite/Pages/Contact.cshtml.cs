1a7c60a53f73e6102573c6fe4c48d8cf3e0791b2
npolyak <nick.polyak@verizon.net>
2025-02-23 15:14:10 -0500

    more changes

---
+++ AspGrpc/AWebProsSite/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWebProsSite.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
