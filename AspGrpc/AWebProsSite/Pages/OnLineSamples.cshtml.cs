72ec223efc074ffeb1a49b75e55ee1a2e3157d9e
npolyak <nick.polyak@verizon.net>
2025-02-23 14:52:38 -0500

    created an operating web site

---
+++ AspGrpc/AWebProsSite/Pages/OnLineSamples.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AWebProsSite.Pages
+{
+    public class OnLineSamplesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AspGrpc/AWebProsSite/wwwroot/dist/client.js
@@ -0,0 +1,6 @@
+﻿const { HelloRequest, HelloReply } = require('./Greeter_pb.js');
+const { GreeterClient } = require('./Greeter_grpc_web_pb.js');
+
+global.HelloRequest = HelloRequest;
+global.HelloReply = HelloReply;
+global.GreeterClient = GreeterClient;
